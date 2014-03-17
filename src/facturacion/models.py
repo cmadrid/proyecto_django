60d667b26c3dbc1727cbed0a3a87823b54cd87cd
¨Ruben <rubenandres92@gmail.com>
2014-03-17 13:55:08 -0500

    agregando otros archivos

---
+++ src/facturacion/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Marca(models.Model):
+    nombre  =   models.CharField(max_length=50)
+    codigo_propio   =   models.Model(max_length=50)
+    
+class GrupoItem(models.Model):
+    nombre  =   models.CharField(max_length=50)
+    
+class Bodega(models.Model):
+    nombre  = models.CharField(max_length=50)
+    direccion   = models.Model(max_length=50)
+    
+    
+
+class Item(models.Model):
+    codigo_barras   = models.CharField(max_length=50)
+    codigo_propio   = models.CharField(max_length=30)
+    descripcion     = models.CharField(max_length=200)
+    es_bien         = models.BooleanField(initial=False)
+    es_servicio     = models.BooleanField(initial=False)
+    pct_utilidad    = models.FloatField()
+    
